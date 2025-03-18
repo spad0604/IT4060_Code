b0113089da164d66370384a0f640114bbf6af1ef
spad0604 <giap.nv225126@sis.hust.edu.vn>
2025-03-18 09:45:44 +0700

    Resolve domain with UDP Protocol

--- LTM_VS/LTM_VS/LTM_VS.cpp
+++ LTM_VS/LTM_VS/LTM_VS.cpp
@@ -53,9 +53,42 @@ int main() {
         inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, sizeof(clientIP));
         clientPort = ntohs(clientAddr.sin_port);
 
-        cout << "Received from " << clientIP << ":" << clientPort << " -> " << buff << '\n';
+        cout << "Received from " << clientIP << ":" << clientPort << " -> " << buff << ': ';
+
+        struct addrinfo hints, *res, *p;
+        char ipStr[INET6_ADDRSTRLEN];
+        ZeroMemory(&hints, sizeof(hints));
+       
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        if (getaddrinfo(buff, NULL, &hints, &res) != 0) {
+            cout << "Failed to resolve domain: " << buff << '\n';
+        }
+        else {
+            cout << "Resolved IP addresses for " << buff << '\n';
+
+            for (p = res; p != NULL; p = p->ai_next) {
+                void* addr;
+                if (p->ai_family == AF_INET) { //IPv4
+                    struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+                    addr = &(ipv4->sin_addr);
+                }
+                else { //IPv6
+                    struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
+                    addr = &(ipv6->sin6_addr);
+                }
+
+                inet_ntop(p->ai_family, addr, ipStr, sizeof(ipStr));
+                std::cout << ipStr << "\n";
+            }
+        }
 
         sendto(server, buff, ret, 0, (sockaddr*)&clientAddr, sizeof(clientAddr));
+        if (res != NULL) {
+            freeaddrinfo(res);
+        }
     }
 
     closesocket(server);
