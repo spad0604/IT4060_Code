c36d8245760fbce5df5e2493411f93f0f48ad87e
spad0604 <giap.nv225126@sis.hust.edu.vn>
2025-03-19 00:05:23 +0700

    update received from server

--- LTM_VS/LTM_VS/LTM_VS.cpp
+++ LTM_VS/LTM_VS/LTM_VS.cpp
@@ -1,5 +1,6 @@
 #define _WIN32_WINNT 0x0600 
 #include <iostream>
+#include<string.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
@@ -10,6 +11,7 @@ using namespace std;
 int main() {
     WSADATA wsaData;
     WORD wVersion = MAKEWORD(2, 2);
+    string response;
 
     if (WSAStartup(wVersion, &wsaData)) {
         cout << "Version is not supported" << endl;
@@ -43,6 +45,10 @@ int main() {
     int clientAddrLen = sizeof(clientAddr);
 
     while (true) {
+        struct addrinfo hints, * res, * p;
+        char ipStr[INET6_ADDRSTRLEN];
+        ZeroMemory(&hints, sizeof(hints));
+
         ret = recvfrom(server, buff, sizeof(buff) - 1, 0, (sockaddr*)&clientAddr, &clientAddrLen);
         if (ret == SOCKET_ERROR) {
             cout << "Error :" << WSAGetLastError() << '\n';
@@ -53,11 +59,7 @@ int main() {
         inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, sizeof(clientIP));
         clientPort = ntohs(clientAddr.sin_port);
 
-        cout << "Received from " << clientIP << ":" << clientPort << " -> " << buff << ': ';
-
-        struct addrinfo hints, *res, *p;
-        char ipStr[INET6_ADDRSTRLEN];
-        ZeroMemory(&hints, sizeof(hints));
+        cout << "Received from " << clientIP << ":" << clientPort << " -> " << buff << '\n';
        
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
@@ -74,6 +76,8 @@ int main() {
                 if (p->ai_family == AF_INET) { //IPv4
                     struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
                     addr = &(ipv4->sin_addr);
+
+
                 }
                 else { //IPv6
                     struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
@@ -82,7 +86,10 @@ int main() {
 
                 inet_ntop(p->ai_family, addr, ipStr, sizeof(ipStr));
                 std::cout << ipStr << "\n";
+				response += string(ipStr) + "\n";
             }
+
+            sendto(server, response.c_str(), response.size(), 0, (sockaddr*)&clientAddr, sizeof(clientAddr));
         }
 
         sendto(server, buff, ret, 0, (sockaddr*)&clientAddr, sizeof(clientAddr));
